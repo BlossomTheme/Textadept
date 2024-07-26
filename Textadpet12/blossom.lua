74472eb6a240fb3d8fe35f2a6b11699ded207c06
J.M. Shamal Lakshan Perera <shamallakshan00600@gmail.com>
2024-07-26 10:40:20 +0530

    base colors

---
+++ Textadpet12/blossom.lua
@@ -0,0 +1,30 @@
+local view, colors, styles = view, view.colors, view.styles
+
+colors.fg1       = 0x8d05ff
+colors.fg2       = 0x8205eb
+colors.fg3       = 0x7604d6
+colors.fg4       = 0x6b04c2
+colors.fg6       = 0xa841ff
+colors.bg1       = 0x1b1110
+colors.bg2       = 0x2d2423
+colors.bg3       = 0x3f3736
+colors.bg4       = 0x524a49
+colors.builtin   = 0xd2b326
+colors.keyword   = 0x006ef4
+colors.const     = 0x8f74ea
+colors.comment   = 0xe2e234
+colors.func      = 0xff0092
+colors.str       = 0x55ff55
+colors.type      = 0xcf9f72
+colors.var       = 0x00acf5
+colors.selection = 0xff0000
+colors.warning   = 0x4700f7
+colors.warning2  = 0x3000ff
+
+-- Normal colors.
+-- Used by some modules, like file-diff or lsp
+colors.red = 0x000099
+colors.orange = 0x0066CC
+colors.yellow = 0x009999
+colors.green = 0x009900
+colors.blue = 0xCC6600
